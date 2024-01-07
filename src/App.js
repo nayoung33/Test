3c6be76515cd276d34c4468eef76d375a8891967
nayoung33 <ilnayungli@gmail.com>
2024-01-07 18:41:51 +0900

    React 생성

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Top from './component/top/Top';
+import Mid from './component/mid/Mid';
+import Bottom from './component/bottom/Bottom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <Top></Top>
+      <Mid></Mid>
+      <Bottom></Bottom>
     </div>
   );
 }
---
+++ src/component/bottom/Bottom.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './styles.css'
+
+const Bottom = () => {
+  return (
+    <div className='bottom'>Bottom</div>
+  )
+}
+
+export default Bottom
