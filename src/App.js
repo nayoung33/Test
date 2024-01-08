7ab01cf03287ddc3aec0f60f040e8f23872cca64
nayoung33 <ilnayungli@gmail.com>
2024-01-08 21:24:55 +0900

    react.1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import './App.css';
-import Top from './component/top/Top';
-import Mid from './component/mid/Mid';
-import Bottom from './component/bottom/Bottom';
+import React, {useState} from 'react';
 
 function App() {
-  return (
-    <div>
+    const [usename, setUsername] = useState('');
+    const [Password,setpassword] = useState('');
+    const onSubmit = (event) => {
+        event.preventDefault();
+      console.log(usename, Password);
+    };
 
-      <Top></Top>
-      <Mid></Mid>
-      <Bottom></Bottom>
-    </div>
-  );
+    return (
+        <div className="App">
+            <form onSubmit={onSubmit}>
+            <input placeholder="Username" value={usename} onChange={(e) => setUsername(e.target.value)}/> <br/>
+            <input placeholder="Password" value={Password}onChange={(e) => setpassword(e.target.value)}/> <br/>
+            <button type="submit">Login</button>
+            </form>
+        </div>
+    );
 }
-
 export default App;
