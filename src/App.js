79a24d2b3d7bfb7e4e098e92f9fe589e2bdfd026
nayoung33 <ilnayungli@gmail.com>
2024-01-09 11:42:56 +0900

    App.2

--- src/App.js
+++ src/App.js
@@ -1,21 +1,50 @@
-import React, {useState} from 'react';
+import React, {Component} from "react";
+import './App.css';
 
-function App() {
-    const [usename, setUsername] = useState('');
-    const [Password,setpassword] = useState('');
-    const onSubmit = (event) => {
-        event.preventDefault();
-      console.log(usename, Password);
-    };
+class Sudject extends Component {
+render() {
+return(
+    <header>
+    <h1>WEB</h1>
+     world wide web
+    </header>
+    );  
+}   
+}
+class TOC extends Component {
+    render(){
+        return (
+            <nav>
+        <ul>
+            <li><a href="1.html">HTML</a></li>
+            <li><a href="2.html">CSS</a></li>
+            <li><a href="3.html">JavaScript</a></li>
+        </ul>
+        </nav>
+        );
+    }
+}
+class Content extends Component {
+    render() {
+        return (
+            <article>
+            <h2>HTML</h2>
+            HTML is HyperText Markup Language.
+            </article>
+        );
+    }
+}
 
-    return (
-        <div className="App">
-            <form onSubmit={onSubmit}>
-            <input placeholder="Username" value={usename} onChange={(e) => setUsername(e.target.value)}/> <br/>
-            <input placeholder="Password" value={Password}onChange={(e) => setpassword(e.target.value)}/> <br/>
-            <button type="submit">Login</button>
-            </form>
-        </div>
-    );
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <Sudject></Sudject>
+                <TOC></TOC>
+                <Content></Content>
+                Hello,React!!
+            </div>
+        );
+    }
 }
 export default App;
